ff9e7792d63200eb4d179ed9312b8734c269d90d
lti <landry.tiehi@gmail.com>
2016-11-17 23:24:09 +0100

    ajout spring boot (dependence et configuration)

---
+++ src/main/java/fr/esiee/pic/esieeLabProductionCore/Application.java
@@ -0,0 +1,19 @@
+package fr.esiee.pic.esieeLabProductionCore;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+ 
+    static final Logger logger = LogManager.getLogger(Application.class.getName());
+    
+    public static void main(String[] args) {
+        logger.info("entered application");
+        
+        SpringApplication.run(Application.class, args);
+    }
+}
