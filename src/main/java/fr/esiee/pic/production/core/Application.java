061f69f5aa317551c16a25d3ae8b4e67c81cb780
lti <landry.tiehi@gmail.com>
2016-11-23 00:30:06 +0100

    Commentaire classe Application

--- src/main/java/fr/esiee/pic/production/core/Application.java
+++ src/main/java/fr/esiee/pic/production/core/Application.java
@@ -6,11 +6,19 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Demarrage de l'application
+ * @author etudiant
+ *
+ */
 @SpringBootApplication
 public class Application {
  
     static final Logger logger = LogManager.getLogger(Application.class.getName());
     
+    /**
+     * Main class
+     */
     public static void main(String[] args) {
         logger.info("entered application");
         
